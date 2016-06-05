c4c8ff9b73eeb168bc03feff1944c9cdd67bc067
Ankit kumar <ankit0308630@gmail.com>
2016-06-05 15:59:45 +0000

    System info and kernel version

---
+++ systeminfo.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Detecting the base operating system"
+cat /etc/redhat-release
+echo ""
+echo ""
+echo "Detecting the kernel version"
+uname -a
+echo ""
+echo ""
+echo "Task Completed successfully"
